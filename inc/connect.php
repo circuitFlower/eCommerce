27afbdad332f356ea54922920caaf7e6eccd2d47
Amanda Ervin <aervin@mac-aervin-2.local>
2015-02-01 08:51:40 -0500

    pword change
    for security purposes changed my pword info

--- inc/connect.php
+++ inc/connect.php
@@ -1,7 +1,7 @@
 <?php
-$localhost = 'localhost';
-$username = 'root';
-$password = 'root';
+$localhost = "your localhost name";
+$username = "your username";
+$password = "your password";
 $connect = mysql_connect($localhost, $username, $password);
 if(! $connect ){
   die('Could not connect: ' . mysql_error());
